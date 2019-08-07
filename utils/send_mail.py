c250fab1ccbe011225d65181f56ae93cb20778c1
Manoj Sukhavasi <manoj.sukhavasi1@gmail.com>
2019-08-07 18:01:14 +0530

    Adding bcc to me and anvesh to monitor the demo requests.

--- utils/send_mail.py
+++ utils/send_mail.py
@@ -1,4 +1,4 @@
-import os,smtplib
+import os,smtplib,argparse
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -14,13 +14,26 @@ def send_mail(email, video_link):
     s.ehlo()
     s.login(MY_ADDRESS, PASSWORD)
 
+    me = MY_ADDRESS
+    to = email
+    bcc = "manoj.sukhavasi1@gmail.com, anvesh@greensight.ai"
+    rcpt = bcc.split(',') + [to]
+
     msg = MIMEMultipart()
-    msg['From']=MY_ADDRESS
     msg['To']=email
     msg['Subject']="Greensight: Please find the attached processed video link"
     body = f'Hi,\n\n\n    Please download the processed video and top 10 highlight clips at {video_link}\n\n    Please feel free to give feedback/suggestions by replying to this email.'
     msg.attach(MIMEText(body))
 
-    s.send_message(msg)
+    #s.send_message(msg)
+    s.sendmail(me, rcpt, msg.as_string())
 
     s.quit()
+
+if __name__=="__main__":
+    parser  = argparse.ArgumentParser()
+
+    parser.add_argument('-m', '--mail', help='Youtube url')
+
+    args = parser.parse_args()
+    send_mail(args.mail, 'Sample')
